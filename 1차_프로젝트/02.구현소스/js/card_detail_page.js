ee152a6ed00a4595214467b77cf57d7413151cea
Tom B. Parker <tombap8@hotmail.com>
2024-12-24 15:03:27 +0900

    Update card_detail_page.js

--- 1차_프로젝트/02.구현소스/js/card_detail_page.js
+++ 1차_프로젝트/02.구현소스/js/card_detail_page.js
@@ -6,34 +6,34 @@ console.log("card_detail_page", myFn);
 // ///////선택된 카드 바꾸는 기능
 // // 카드 객체 값\
 let popCard = {
-  Graffiti: [
+  graffiti: [
     "./imgs/navercard/img_navercard_1.png",
-    "../imgs/navercard/img_navercard_back_1.png",
+    "./imgs/navercard/img_navercard_back_1.png",
   ],
 
   line: [
     "./imgs/navercard/img_navercard_2.png",
-    "../imgs/navercard/img_navercard_back_2.png",
+    "./imgs/navercard/img_navercard_back_2.png",
   ],
 
   type: [
-    "../imgs/navercard/img_navercard_3.png",
-    "../imgs/navercard/img_navercard_back_3.png",
+    "./imgs/navercard/img_navercard_3.png",
+    "./imgs/navercard/img_navercard_back_3.png",
   ],
 
   joy: [
-    "../imgs/navercard/img_navercard_4.png",
-    "../imgs/navercard/img_navercard_back_4.png",
+    "./imgs/navercard/img_navercard_4.png",
+    "./imgs/navercard/img_navercard_back_4.png",
   ],
 
   tube: [
-    "../imgs/navercard/img_navercard_5.png",
-    "../imgs/navercard/img_navercard_back_5.png",
+    "./imgs/navercard/img_navercard_5.png",
+    "./imgs/navercard/img_navercard_back_5.png",
   ],
 
   core: [
-    "../imgs/navercard/img_navercard_6.png",
-    "../imgs/navercard/img_navercard_back_6.png",
+    "./imgs/navercard/img_navercard_6.png",
+    "./imgs/navercard/img_navercard_back_6.png",
   ],
 };
 
@@ -58,10 +58,15 @@ var swiper = new Swiper(".mySwiper", {
 // 1. 팝업 숨기기
 $(".select-card-popup").hide();
 
+// popCard객체의 배열화!
+const keyPopCard = Object.keys(popCard);
+console.log(keyPopCard);
+
 // // 2. 디테일 카드 클릭시 팝업 나오기
 $(".view-card-detail").click((idx, e) => {
   $(".swiper-slide").each((idx, el) => {
-    let hCode = [
+    console.log(idx,el,keyPopCard[idx]);
+    let hCode = 
       `
   <div class="popup-wrap">
     <h2 class="blind">선택카드디테일 팝업페이지</h2>
@@ -69,16 +74,16 @@ $(".view-card-detail").click((idx, e) => {
       <div class="col-1"></div>
       <div class="col-10">
         <section class="card-name-sec">
-          <p></p>
+          <p>${keyPopCard[idx]}</p>
           <p>플라스틱플레이트</p>
         </section>
         <section class="card-detail-sec">
           <div class="card-detail-sec-fornt">
-           <img src="">
+           <img src="${popCard[keyPopCard[idx]][0]}">
             <aside>-앞면-</aside>
           </div>
           <div class="card-detail-sec-back">
-          <img src="">
+          <img src="${popCard[keyPopCard[idx]][1]}">
             <aside>-뒷면-</aside>
           </div>
         </section>
@@ -97,10 +102,10 @@ $(".view-card-detail").click((idx, e) => {
       </div>
       <div class="col-1"></div>
     </div>
-  </div>`,
-    ];
+  </div>`
+    ;
     // html 넣기
-    $(".swiper-slide").html(hCode);
+    $(el).html(hCode);
   }); ///코드 넣기 ///
   $(".select-card-popup").fadeIn(400);
 });
@@ -111,29 +116,33 @@ $("#close-button").click(() => {
 });
 
 // '.view-card-detail 클릭시 span 값 불러오기
-$(".view-card-detail").click(function () {
-  // text 값 불러오기
+// $(".view-card-detail").click(function () {
+//   // text 값 불러오기
 
-  let txt = "";
+//   let txt = "";
 
-  // txt값 할당하기
-  txt = $(this, "span").text();
-  // console.log(txt);
+//   // txt값 할당하기
+//   txt = $(this, "span").text().trim().toLowerCase();
+//   console.log(txt);
 
-  // txt 값 넣기
-  $(".card-name-sec p").first().text(txt);
+//   // txt 값 넣기
+//   $(".card-name-sec p").first().text(txt);
 
-  // txt 값에 따라 이미지 넣기
+//   // txt 값에 따라 이미지 넣기
 
-  //  스위치문 이용하여 값 넣기
-  switch (txt) {
-    case Graffiti:
-      $(".card-detail-sec-fornt").find("img").attr("src");
-      $(".card-detail-sec-back").find("img").attr("src", popCard.Graffiti[0]);
-      break;
-  }
-  console.log(popCard.Graffiti[0]);
+  
+//       $(".card-detail-sec-fornt").find("img").attr("src", popCard[txt][0]);
+//       $(".card-detail-sec-back").find("img").attr("src", popCard[txt][1]);
 
-  // 팝업 보이기
-  $(".select-card-popup").fadeIn(400);
-});
+//   //  스위치문 이용하여 값 넣기
+//   // switch (txt) {
+//   //   case Graffiti:
+//   //     $(".card-detail-sec-fornt").find("img").attr("src");
+//   //     $(".card-detail-sec-back").find("img").attr("src", popCard.Graffiti[0]);
+//   //     break;
+//   // }
+//   // console.log(popCard.Graffiti[0]);
+
+//   // 팝업 보이기
+//   $(".select-card-popup").fadeIn(400);
+// });
